aec7ada620d15c04eacf30748be67baed97286c7
skuderski <skuderski@gmail.com>
2024-10-20 21:33:40 +0200

    practice javascript

---
+++ javascript.js
@@ -0,0 +1,47 @@
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+let result = 5 > 4;
+alert(result)
+
+alert( '1' > 1 );
+
+let surname = "Doe";
+console.log(surname);
+
+let age = "11";
+console.log(age);
+
+age = "54";
+console.log(age);
+
+const pi = "3.14";
+console.log(pi)
+
+console.log((3 + 2) - 76 * (1 + 1))
+
+console.log(23+97)
+
+console.log(5+54+73+22+90)
+
+console.log((4 + 6 + 9) / 77)
+
+let title = prompt("What's the official name of Javascript?", "");
+if (title == "ECMAScript") {
+    alert("Right!")}
+else {
+    alert("You don't know? ECMAScript")
+}
+
+let number = prompt("Type a number", 0)
+
+if (number > 0) {
+    alert(1);
+}
+else if (number < 0) {
+    alert(-1);
+}
+else  {
+    alert(0);
+}
